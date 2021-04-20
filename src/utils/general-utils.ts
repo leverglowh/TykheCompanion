c1be2e2d20e1e775690f18f30ccbea7ca46c6e8a
leverglowh <leverglowh@gmail.com>
2021-04-20 17:07:58 +0200

    Add :3_star: format emote support

--- src/utils/general-utils.ts
+++ src/utils/general-utils.ts
@@ -1,6 +1,6 @@
 import { ICard } from "model/card.model";
 
-export const CARD_INPUT_REGEX = /(^:[1-5]star128:\W*-\W*(.+)\W*-\W*[0-9]{4}$)|(^[1-5](s|star)?[\s-]{1}(.+)[\s-]{1}[0-9]{4}$)/;
+export const CARD_INPUT_REGEX = /(^:[1-5](star128|_star):\W*-\W*(.+)\W*-\W*[0-9]{4}$)|(^[1-5](s|star)?[\s-]{1}(.+)[\s-]{1}[0-9]{4}$)/;
 
 export const sortCardByNameAscTierDiscUIDAsc = (card1: ICard, card2: ICard) => {
   if (card1.name === card2.name) {
@@ -15,7 +15,7 @@ export const sortCardByNameAscTierDiscUIDAsc = (card1: ICard, card2: ICard) => {
     if (card1.name < card2.name) {
       return -1;
     } else {
-      return 1;
+      return 1; 
     }
   }
 };
@@ -26,7 +26,7 @@ export const parseCard = (line: string) => {
   let name = "";
   if (line?.[0] === ":") {
     // copied from inv
-    if (!/^:[0-5]star128:/.test(line)) return null;
+    if (!/^:[0-5](star128|_star):/.test(line)) return null;
 
     tier = Number(line[1]);
     const temp_vals = [
